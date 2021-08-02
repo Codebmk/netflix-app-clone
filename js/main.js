2193cd0e2a9038191d89eac0a4d1c93976c9c7ce
Codebmk <belindamarion0@gmail.com>
2021-08-02 21:45:13 +0300

    fixed scrollbar width, fixed navbar padding, changed movie item image size for mobile, fixed scrollbar

--- js/main.js
+++ js/main.js
@@ -2,21 +2,22 @@ import moviesList from "../data-store/movies.js";
 import movieGenresData from "../data-store/genres.js";
 const moviesListDiv1 = document.querySelector(".movies-list-1");
 const moviesListDiv2 = document.querySelector(".movies-list-2");
-const movieGenres = document.querySelector(".genres-list");
+const movieGenres = document.querySelectorAll(".genres-list");
 
-movieGenresData.forEach(genre => {
+movieGenres.forEach(elem => {
+  movieGenresData.forEach(genre => {
     const genreItem = document.createElement("li");
     genreItem.className = "genres-list-item";
     genreItem.textContent = genre;
-    movieGenres.appendChild(genreItem);
+    elem.appendChild(genreItem);
+  });
 });
 
-
 // add movie items to section 1
-for (let i = 0; i <= Math.floor(moviesList.length / 2); i++){
-    const movieItemDiv = document.createElement("div");
-    movieItemDiv.className = "movie-list-item";
-    movieItemDiv.innerHTML = `
+for (let i = 0; i <= Math.floor(moviesList.length / 2); i++) {
+  const movieItemDiv = document.createElement("div");
+  movieItemDiv.className = "movie-list-item";
+  movieItemDiv.innerHTML = `
         <img src="../assets/${moviesList[i].img}" alt="${moviesList[i].title}" class="movie-img"/>
         <div class="movie-info">
             <h3 class="movie-title">${moviesList[i].title}</h3>
@@ -30,14 +31,18 @@ for (let i = 0; i <= Math.floor(moviesList.length / 2); i++){
             </div>
         </div>
     `;
-    moviesListDiv1.appendChild(movieItemDiv);
+  moviesListDiv1.appendChild(movieItemDiv);
 }
 
 // add movie items to section 2
-for (let i = moviesList.length-1; i > Math.floor(moviesList.length / 2); i--){
-    const movieItemDiv = document.createElement("div");
-    movieItemDiv.className = "movie-list-item";
-    movieItemDiv.innerHTML = `
+for (
+  let i = moviesList.length - 1;
+  i > Math.floor(moviesList.length / 2);
+  i--
+) {
+  const movieItemDiv = document.createElement("div");
+  movieItemDiv.className = "movie-list-item";
+  movieItemDiv.innerHTML = `
         <img src="assets/${moviesList[i].img}" alt="${moviesList[i].title}" class="movie-img"/>
         <div class="movie-info">
             <h3 class="movie-title">${moviesList[i].title}</h3>
@@ -51,9 +56,9 @@ for (let i = moviesList.length-1; i > Math.floor(moviesList.length / 2); i--){
             </div>
         </div>
     `;
-    moviesListDiv2.appendChild(movieItemDiv);
+  moviesListDiv2.appendChild(movieItemDiv);
 }
 
 document.querySelector("#nav-menu").addEventListener("click", () => {
-    document.querySelector("#nav-links").classList.toggle("nav-link-toggle-show");
-})
+  document.querySelector("#nav-links").classList.toggle("nav-link-toggle-show");
+});
